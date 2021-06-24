96d1c8eacb46e33cab11529dc3e76a87971fb179
BernieWhite <BernieWhite@users.noreply.github.com>
2021-06-24 05:07:28 +0000

    deploy: abd972f55dd683cbd844b4116bdfd800b1bc7f43

---
+++ hooks.py
@@ -0,0 +1,24 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import mkdocs.config
+import mkdocs.config.config_options
+import mkdocs.structure.files
+import mkdocs.structure.nav
+import mkdocs.structure.pages
+
+# Dynamically build template nav
+def build_template_nav(nav: mkdocs.structure.nav.Navigation, config: mkdocs.config.Config, files: mkdocs.structure.files.Files) -> mkdocs.structure.nav.Navigation:
+    children = []
+    item: mkdocs.structure.nav.Section = mkdocs.structure.nav.Section("Templates", children)
+
+    for f in files:
+        if not f.is_documentation_page():
+            continue
+
+        if f._get_dest_path(False).__contains__("/templates/"):
+            children.append(mkdocs.structure.pages.Page(f._get_stem(), f, config))
+
+    nav.items.append(item)
+    mkdocs.structure.nav._add_parent_links(nav)
+    return nav
